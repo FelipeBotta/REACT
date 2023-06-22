2933c05ff4e164a82b78062647a50567582cc5bd
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-06-22 15:25:45 -0300

    Update main.js

--- Alura Midi/main.js
+++ Alura Midi/main.js
@@ -1,12 +1,25 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-
+function tocaSom (idEelementoAudio) {
+    document.querySelector(idEelementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
 
+while (contador < listaDeTeclas.length) {
 
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    console.log(instrumento);
+    
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom()
+    }
+
+    contador = contador + 1;
+
+    console.log(contador)
+}
 
 
 
