d8c08445e91e7494be852e1df8dcc9cb9860e6e2
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-06-22 16:02:47 -0300

    Update main.js

--- Alura Midi/main.js
+++ Alura Midi/main.js
@@ -17,8 +17,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     tecla.onclick = function (){
         tocaSom(idAudio)
+        
     }
-    
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
 }
 
 
