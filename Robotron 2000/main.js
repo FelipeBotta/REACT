b833bf3d29e3ed4557c72571a741ed576f039db5
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-06-23 16:10:31 -0300

    Js Update

--- Robotron 2000/main.js
+++ Robotron 2000/main.js
@@ -1,14 +1,21 @@
-const  robotron = document.querySelector('#robotron')
+const  subtrair = document.querySelector('#subtrair')
+const  somar = document.querySelector('#somar')
 
-robotron.addEventListener ("click" , (evento) => {
-    console.log(evento)
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
 })
 
-function dizOi (nome){
-console.log("Oi " + nome)
-console.log("Bem-vindo ao Robotron 2000")
 
+function manipulaDados (operacao, controle){
+    const peca = controle.querySelector(".controle-contador")
 
+    if(operacao === "-" ){
+        peca.value = parseInt(peca.value) - 1
+    } else{
+        peca.value = parseInt(peca.value) + 1
+    }
 }
-
-dizOi("Pedro")
